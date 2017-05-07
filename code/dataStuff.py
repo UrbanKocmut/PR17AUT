687069bc9a87b6237a937e7f3718d6aacaccd722
UrbanKocmut <u.kocmut@gmail.com>
2017-05-07 09:55:48 +0200

    Pretvoril datoteke v UTF-8

--- code/dataStuff.py
+++ code/dataStuff.py
@@ -1,15 +1,23 @@
 
-def merge():
-    fout=open("../data/2016/podatki_2016.csv","a")
+"""
+Združi podatke po standardni obliki
+"""
+def merge(leto):
+    leto = str(leto)
+    fout=open("../data/"+leto+"/podatki_"+leto+".csv","a")
     # first file:
-    for line in open("../data/2016/NPodatki_012016.csv"):
+    for line in open("../data/"+leto+"/NPodatki_01"+leto+".csv", encoding="UTF-16LE"):
         fout.write(line)
     # now the rest:
     for num in range(2,13):
-        f = open("../data/2016/NPodatki_"+"{:02d}".format(num)+"2016.csv")
+        f = open("../data/"+leto+"/NPodatki_"+"{:02d}".format(num)+leto+".csv", encoding="UTF-16LE")
         next(f) # skip the header
         print(num)
         for line in f:
              fout.write(line)
         f.close() # not really needed
     fout.close()
+
+merge(2014)
+merge(2015)
+merge(2016)
