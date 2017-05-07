d5d15edfadda417e1fa2baebed4aeaa9a1c15770
UrbanKocmut <u.kocmut@gmail.com>
2017-05-07 10:18:11 +0200

    Dodal podatke za januar 2017

--- code/dataStuff.py
+++ code/dataStuff.py
@@ -1,23 +1,24 @@
 
+
 """
-Združi podatke po standardni obliki
+Združi podatke po standardni obliki, če je bog dal blagoslov
 """
 def merge(leto):
     leto = str(leto)
-    fout=open("../data/"+leto+"/podatki_"+leto+".csv","a")
+    fout = open("../data/" + leto + "/podatki_" + leto + ".csv", "a")
     # first file:
-    for line in open("../data/"+leto+"/NPodatki_01"+leto+".csv", encoding="UTF-16LE"):
+    for line in open("../data/" + leto + "/NPodatki_01" + leto + ".csv"):
         fout.write(line)
     # now the rest:
-    for num in range(2,13):
-        f = open("../data/"+leto+"/NPodatki_"+"{:02d}".format(num)+leto+".csv", encoding="UTF-16LE")
-        next(f) # skip the header
+    for num in range(2, 13):
+        f = open("../data/" + leto + "/NPodatki_" + "{:02d}".format(num) + leto + ".csv", encoding="UTF-8")
+        next(f)  # skip the header
         print(num)
         for line in f:
-             fout.write(line)
-        f.close() # not really needed
+            fout.write(line)
+        f.close()  # not really needed
     fout.close()
 
-merge(2014)
-merge(2015)
-merge(2016)
+
+
+
