e15ac41806b486814e2b4bd0b0cc0fc1d9cd54c2
UrbanKocmut <u.kocmut@gmail.com>
2017-05-07 11:23:18 +0200

    Dodal metodo za pretvorbo datoteke v dict

--- code/dataStuff.py
+++ code/dataStuff.py
@@ -1,8 +1,11 @@
-
+import pandas as pd
+from csv import DictReader
 
 """
 Združi podatke po standardni obliki, če je bog dal blagoslov
 """
+
+
 def merge(leto):
     leto = str(leto)
     fout = open("../data/" + leto + "/podatki_" + leto + ".csv", "a")
@@ -20,5 +23,13 @@ def merge(leto):
     fout.close()
 
 
-
+def getDict(leto, mesec=1):
+    leto = str(leto)
+    file = "../data/" + leto + "/NPodatki_"+"{:02d}".format(mesec) + leto + ".csv"
+    dict_list = []
+    with open(file) as csvfile:
+        reader = DictReader(csvfile, delimiter=';')
+        for row in reader:
+            dict_list.append(row)
+    return dict_list
 
